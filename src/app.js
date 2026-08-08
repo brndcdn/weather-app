69a6d2453cd7c315133da1a2dd852b0fdd16ab9f
brndcdn <brendacadena.design@gmail.com>
2026-08-08 12:31:26 +0200

    Added the search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city-heading");
+  let temperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "9215d217o28938dd2t4a9f123417b908";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city-heading");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Bogota");
